19bdaa7f0179570efd92a2318e8fd08d7cc4eb60
zohdi <cnthope24@gmail.com>
2026-06-22 03:13:48 -0700

    commityuly

--- healers.cpp
+++
@@ -1,9 +0,0 @@
-#include "hero.hpp"
-#include "healers.hpp"
-#include <iostream>
-
-using namespace std;
-
-Healer::Healer(string name, int hp, int specialPower):Hero(name,"Healer",hp,specialPower)
-{
-}
